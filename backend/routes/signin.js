6bca363a5edc7ba990d37ce36c07eca623d225c1
Artem Bulychev <twist1996@icloud.com>
2023-08-20 16:45:45 +0500

    commit file 15PR v 2.6.1

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -8,7 +8,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
+      password: Joi.string().required(),
     }),
   }),
   login,
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -9,7 +9,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
+      password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().pattern(urlRegex),
